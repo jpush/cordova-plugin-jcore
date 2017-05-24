7c6cd7c5ab1efc78f160e34677f25a1b539eeb4b
hevin <hev1n@qq.com>
2017-05-24 13:51:27 +0800

    Update version number

--- www/jcore.js
+++ www/jcore.js
@@ -1,5 +1,5 @@
-var exec = require('cordova/exec');
+var exec = require('cordova/exec')
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "jcore", "coolMethod", [arg0]);
-};
+exports.coolMethod = function (arg0, success, error) {
+  exec(success, error, 'jcore', 'coolMethod', [arg0])
+}
